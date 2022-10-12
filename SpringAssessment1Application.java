3811115d3fb97a79bbe54c31cc4df141e1cdf238
1ruthvik <113304548+1ruthvik@users.noreply.github.com>
2022-10-12 17:48:18 +0530

    Add files via upload

---
+++ SpringAssessment1Application.java
@@ -0,0 +1,13 @@
+package com.gl.springAssessment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssessment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssessment1Application.class, args);
+	}
+
+}
